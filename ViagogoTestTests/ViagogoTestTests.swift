db918e5858b26a79a1cd071bb0e1fa3753e89333
Alessandro Manni <ale_manni@icloud.cm>
2016-07-15 15:44:03 +0100

    Unit tests

--- ViagogoTestTests/ViagogoTestTests.swift
+++ ViagogoTestTests/ViagogoTestTests.swift
@@ -13,23 +13,76 @@ class ViagogoTestTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let story = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+        let navController = story.instantiateInitialViewController() as! UINavigationController
+        let countryListVC = navController.viewControllers[0] as! CountryListViewController
+        let _ = countryListVC.view
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testShowError() {
+        let networkError = NSError(domain: "https://restcountries.eu/", code: 404, userInfo: [NSLocalizedDescriptionKey: "The requested URL was not found on this server"])
+        let errorArray = [Errors.jsonError, Errors.noData, Errors.networkError(nsError: networkError)]
+        let errorModel = ErrorHandler()
+        for error in errorArray {
+            switch error {
+            case .jsonError:
+                errorModel.showError(error, sender: UIViewController())
+                let textLabel = errorModel.alert!.subTextLabel.text
+                XCTAssertEqual(textLabel, "Response from server cannot be converted in readable data")
+                errorModel.viewIsShown(false)
+                
+            case .noData:
+                errorModel.showError(error, sender: UIViewController())
+                let  textLabel = errorModel.alert!.subTextLabel.text
+                XCTAssertEqual(textLabel, "No valid data returned from server")
+                errorModel.viewIsShown(false)
+                
+            case .networkError(nsError: networkError):
+                errorModel.showError(error, sender: UIViewController())
+                let  textLabel = errorModel.alert!.subTextLabel.text
+                XCTAssertEqual(textLabel, "The requested URL was not found on this server")
+                errorModel.viewIsShown(false)
+            default:
+                XCTFail("Unexpected error in mock array")
+            }
+        }
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    func testPopulateDataSource() {
+        //Given
+        class TestCountryListViewController: CountryListViewController {
+            var expectationForCallBack: XCTestExpectation?
+            var result: [Country]?
+            
+            override func populateDataSource() {
+                ConnectionManager.fetchAllCountries() { (callback) in
+                    guard callback.error == nil else {
+                        ErrorHandler.handler.showError(callback.error!, sender: self)
+                        self.expectationForCallBack?.fulfill()
+                        return
+                    }
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.result = callback.response! as [Country]
+                        self.expectationForCallBack?.fulfill()
+                        return
+                    }
+                }
+            }
+        }
+        let controller = TestCountryListViewController()
+        //When
+        controller.expectationForCallBack = expectationWithDescription("Finished fetching all countries")
+        controller.populateDataSource()
+        //Then
+        waitForExpectationsWithTimeout(ConnectionManager.session.configuration.timeoutIntervalForResource, handler: { (error) in
+            XCTAssertNil(error, "Timeout error")})
+        XCTAssertNotNil(controller.result)
+        if let testResult = controller.result {
+            XCTAssert(testResult.count > 0, "The result array is empty!")
         }
     }
     
