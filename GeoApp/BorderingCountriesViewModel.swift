bff96ca9733dee4fb4a6e22f00a64ebe40f687f7
Alessandro Manni <ale_manni@icloud.cm>
2017-04-09 12:58:31 +0100

    Completes previous commits with view model classes

---
+++ GeoApp/BorderingCountriesViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BorderingCountriesViewModel.swift
+//  GeoApp
+//
+//  Created by Alessandro Manni on 09/04/2017.
+//  Copyright © 2017 Alessandro Manni. All rights reserved.
+//
+
+import Foundation
+
+final class BorderingCountriesViewModel {
+    
+}
---
+++ GeoApp/CapitalCityViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CapitalCityViewModel.swift
+//  GeoApp
+//
+//  Created by Alessandro Manni on 09/04/2017.
+//  Copyright © 2017 Alessandro Manni. All rights reserved.
+//
+
+import Foundation
+
+final class CapitalCityViewModel {
+    
+}
