54ef43d667c00fd4247ec2842caaffabff1d4c04
Alessandro Manni <ale_manni@icloud.cm>
2017-04-23 20:31:16 +0100

    No bordering countries placeholder

--- GeoApp/BorderingCountriesTableView.swift
+++ GeoApp/BorderingCountriesTableView.swift
@@ -15,16 +15,32 @@ protocol BorderingCountriesTableViewDelegate: class {
 
 final class BorderingCountriesTableView: UIView {
     @IBOutlet weak var tableView: CountryListTableView!
-
     @IBOutlet weak var sectionTitleView: UIView!
+    @IBOutlet weak var sectionTitleViewLabel: UILabel!
 
     weak var delegate: BorderingCountriesTableViewDelegate?
 
+    private var areBordersAvailable: Bool = false {
+        didSet {
+            switch areBordersAvailable {
+            case true:
+                tableView.data = data
+                tableView.setUpView()
+                tableView.delegate = self
+                sectionTitleViewLabel.text = "Bordering Countries"
+                break
+            case false:
+                sectionTitleViewLabel.text = "No Bordering Countries"
+                break
+            }
+        }
+    }
+
     var data: CountryListRepresentable? {
         didSet {
-            tableView.data = data
-            tableView.setUpView()
-            tableView.delegate = self
+            if let data = data {
+                areBordersAvailable = data.count > 0
+            }
         }
     }
 
@@ -34,12 +50,11 @@ final class BorderingCountriesTableView: UIView {
     }
 }
 
-    extension BorderingCountriesTableView: CountryListViewDelegate {
-        func viewDidRequestDataUpdate() {
+extension BorderingCountriesTableView: CountryListViewDelegate {
+    func viewDidRequestDataUpdate() {
 
-        }
-        func viewDidSelectCountry(countryName: String) {
-            delegate?.viewDidSelectCountry(countryName: countryName)
-        }
-        
+    }
+    func viewDidSelectCountry(countryName: String) {
+        delegate?.viewDidSelectCountry(countryName: countryName)
+    }
 }
